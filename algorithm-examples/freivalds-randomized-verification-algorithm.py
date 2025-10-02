c6e8f162dfc78184f4c2d25964779def41b9cc06
kylindreagan <155770203+kylindreagan@users.noreply.github.com>
2025-10-02 02:33:04 -0400

    More accurate name
    Also add accounting for error

--- algorithm-examples/freivalds-randomized-verification-algorithm.py
+++ algorithm-examples/freivalds-randomized-verification-algorithm.py
@@ -11,9 +11,15 @@ import random
 6. If it is zero or false, then matrix multiplication is correct otherwise not.
 7. End"""
 
-def coppersmithWinogradVerify(A, B, C, n):
+def Freivaldsrandverify(A, B, C, n):
     v = [random.randint(0,1) for _ in range(n)]
     Bv = [sum(B[i][k] * v[k] for k in range(n)) for i in range(n)]
     Cv = [sum(C[j][k] * v[k] for k in range(n)) for j in range(n)]
     ABv = [sum(A[h][k] * Bv[k] for k in range(n)) for h in range(n)]
     return all(ABv[i] == Cv[i] for i in range(n))
+
+def verify(A, B, C, n, trials=5):
+    for _ in range(trials):
+        if not Freivaldsrandverify(A, B, C, n):
+            return False
+    return True
